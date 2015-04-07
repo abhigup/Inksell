7d3466ef70207aeabb3a941f8cb1646f377f2602
abhinav <abhigup@live.com>
2015-04-08 01:24:16 +0530

    merge

---
+++ app/src/main/java/utilities/PostResponseStatus.java
@@ -0,0 +1,12 @@
+package utilities;
+
+/**
+ * Created by nikhilg on 8/4/15.
+ */
+public enum PostResponseStatus {
+    PostAddedSuccess,
+    PostSummaryAdded,
+    PostSummaryNotAdded,
+    ImageNotAdded,
+    SomeErrorOccured
+}
